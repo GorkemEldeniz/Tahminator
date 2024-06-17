581fc65ff4b17dfea1719c36be4cace6072754ec
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-06-17 12:30:13 +0300

    fixed webhooks event

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -12,43 +12,37 @@ export async function POST(req: Request) {
 
 	const sivx = new Webhook(webhookSecret);
 
-	let res;
-
 	try {
-		res = sivx.verify(body, {
+		const res = sivx.verify(body, {
 			"svix-id": svix_id,
 			"svix-timestamp": svix_timestamp,
 			"svix-signature": svix_signature,
 		}) as any;
-	} catch (err) {
-		return new Response("Bad Request", { status: 400 });
-	}
 
-	// Save the user to database
+		// Save the user to database
 
-	if (res?.type === "user.created") {
-		const { first_name, id } = res?.data;
+		if (res?.type === "user.created") {
+			const { first_name, id } = res?.data;
 
-		if (typeof first_name === "string" && typeof id === "string") {
 			await prisma.user.create({
 				data: {
 					id,
-					name: first_name,
-					score: 0,
+					name: first_name || "user",
 				},
 			});
-		}
-	} else if (res?.type === "user.deleted") {
-		const { id } = res?.data;
+		} else if (res?.type === "user.deleted") {
+			const { id } = res?.data;
 
-		if (typeof id === "string") {
 			await prisma.user.delete({
 				where: {
 					id,
 				},
 			});
 		}
-	}
 
-	return new Response("OK", { status: 200 });
+		return new Response("OK", { status: 200 });
+	} catch (err) {
+		console.log(err);
+		return new Response("Bad Request", { status: 400 });
+	}
 }
