3a0df59072b2ed047f050aaee5bad3cc16faa319
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-06-21 02:47:55 +0300

    fix

---
+++ src/helpers/schema.ts
@@ -0,0 +1,93 @@
+import { z } from "zod";
+
+const predictionFormSchema = z.object({
+	matchId: z.string(),
+	home: z.string().min(2).max(50),
+	away: z.string().min(2).max(50),
+	homeScore: z.string().transform((val, ctx) => {
+		const parsed = parseInt(val);
+		if (isNaN(parsed)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Not a number",
+			});
+			return z.NEVER;
+		}
+
+		if (parsed < 0 || parsed > 10) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Number must between 0-10",
+			});
+			return z.NEVER;
+		}
+
+		return parsed;
+	}),
+	awayScore: z.string().transform((val, ctx) => {
+		const parsed = parseInt(val);
+		if (isNaN(parsed)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Not a number",
+			});
+			return z.NEVER;
+		}
+
+		if (parsed < 0 || parsed > 10) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Number must between 0-10",
+			});
+			return z.NEVER;
+		}
+
+		return parsed;
+	}),
+});
+
+const predictionUpdateFormSchema = z.object({
+	matchId: z.string(),
+	homeScore: z.string().transform((val, ctx) => {
+		const parsed = parseInt(val);
+		if (isNaN(parsed)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Not a number",
+			});
+			return z.NEVER;
+		}
+
+		if (parsed < 0 || parsed > 10) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Number must between 0-10",
+			});
+			return z.NEVER;
+		}
+
+		return parsed;
+	}),
+	awayScore: z.string().transform((val, ctx) => {
+		const parsed = parseInt(val);
+		if (isNaN(parsed)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Not a number",
+			});
+			return z.NEVER;
+		}
+
+		if (parsed < 0 || parsed > 10) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Number must between 0-10",
+			});
+			return z.NEVER;
+		}
+
+		return parsed;
+	}),
+});
+
+export { predictionFormSchema, predictionUpdateFormSchema };
